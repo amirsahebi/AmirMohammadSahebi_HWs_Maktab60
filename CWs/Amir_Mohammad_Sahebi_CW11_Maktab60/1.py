b430da06dfa28d9d1e2211ebfca08028e1bbba61
zolfaghaar <84781206+zolfaghaar@users.noreply.github.com>
2021-09-23 16:43:09 +0330

    Add files via upload
    adding the first cw11

--- CWs/Amir_Mohammad_Sahebi_CW11_Maktab60/1.py
+++ CWs/Amir_Mohammad_Sahebi_CW11_Maktab60/1.py
@@ -1,1 +1,16 @@
+from pprint import pprint
 
+import requests
+import json
+
+params = {
+    'results': 10
+}
+response = requests.get('https://randomuser.me/api/', params=params)
+result = response.json()
+result_list = []
+
+for i in range(params['results']):
+    result_list.append(result['results'][i]['name']['first'] + " " + result['results'][i]['name']['last'])
+
+print(result_list)
